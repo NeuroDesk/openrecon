d31a2117e71e9d9b2fb47a0d708b976ece234d99
Steffen Bollmann <stebo85@users.noreply.github.com>
2024-06-19 15:41:48 +0200

    try to install older docker version

--- recipes/defacing/build.sh
+++ recipes/defacing/build.sh
@@ -6,6 +6,10 @@ pip3 install packaging
 
 sudo apt install p7zip
 
+sudo apt remove docker
+wget https://desktop.docker.com/linux/main/amd64/131620/docker-desktop-4.26.1-x86_64.rpm
+sudo dpkg -i docker-desktop-4.26.1-x86_64.rpm
+
 python ../checkDockerVersion.py
 
 # build zip file
