fc223f2cf6d6878c1712a682ee48e2de45ebd0d2
Steffen Bollmann <stebo85@users.noreply.github.com>
2024-06-19 15:46:13 +0200

    run on self hosted

--- recipes/defacing/build.sh
+++ recipes/defacing/build.sh
@@ -6,10 +6,6 @@ pip3 install packaging
 
 sudo apt install p7zip
 
-sudo apt remove docker
-wget https://desktop.docker.com/linux/main/amd64/131620/docker-desktop-4.26.1-x86_64.rpm
-sudo dpkg -i docker-desktop-4.26.1-x86_64.rpm
-
 python ../checkDockerVersion.py
 
 # build zip file
