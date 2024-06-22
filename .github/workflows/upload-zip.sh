bd2e600a7c5ad830d52e6ab89f6b74edc21c4131
Steffen Bollmann <stebo85@users.noreply.github.com>
2024-06-22 17:51:32 +0200

    keep file in build directory

--- .github/workflows/upload-zip.sh
+++ .github/workflows/upload-zip.sh
@@ -5,12 +5,10 @@ echo "[DEBUG] recipes/$APPLICATION"
 cd recipes/$APPLICATION
 
 IMAGENAME=$1
-IMAGE_HOME=/storage/openrecon
 
-echo "[DEBUG] IMAGE_HOME: $IMAGE_HOME"
 echo "[DEBUG] IMAGENAME: $IMAGENAME"
 
-ls -la $IMAGE_HOME/$IMAGENAME.zip
+ls -la $IMAGENAME.zip
 
 
 #This prevents the sometimes stuck apt process from stopping the build
@@ -21,16 +19,16 @@ if [ -f "/var/lib/apt/lists/lock" ]; then
 fi
 
 echo "[DEBUG] Attempting upload to AWS Object Storage ..."
-rclone copy --progress $IMAGE_HOME/${IMAGENAME}.zip aws-neurocontainers:/openrecon
+rclone copy --progress ${IMAGENAME}.zip aws-neurocontainers:/openrecon
 
 if curl --output /dev/null --silent --head --fail "https://openrecon.neurodeks.org/${IMAGENAME}.zip"; then
     echo "[DEBUG] ${IMAGENAME}.simg was freshly build and exists now :)"
-    echo "[DEBUG] cleaning up $IMAGE_HOME/${IMAGENAME}.zip"
-    rm -rf $IMAGE_HOME/${IMAGENAME}.zip
+    echo "[DEBUG] cleaning up ${IMAGENAME}.zip"
+    rm -rf ${IMAGENAME}.zip
 else
     echo "[ERROR] ${IMAGENAME}.zip does not exist yet. Something is WRONG"
-    echo "[ERROR] cleaning up $IMAGE_HOME/${IMAGENAME}.zip"
-    rm -rf $IMAGE_HOME/${IMAGENAME}.zip
+    echo "[ERROR] cleaning up ${IMAGENAME}.zip"
+    rm -rf ${IMAGENAME}.zip
     exit 2
 fi
 
