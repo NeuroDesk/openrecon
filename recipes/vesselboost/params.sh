a9b65c70c41d25568fb53072bfaa3e92ef6c6c60
Steffen Bollmann <stebo85@users.noreply.github.com>
2024-10-30 16:24:13 +1000

    vessel boost

--- recipes/vesselboost/params.sh
+++ recipes/vesselboost/params.sh
@@ -1,2 +1,3 @@
+# build image here: https://github.com/NeuroDesk/neurocontainers and add mrd server instructions: https://www.neurodesk.org/docs/getting-started/neurocontainers/openrecon/
 # specify the repostiory and name of the docker image: https://hub.docker.com/orgs/vnmd/repositories
 export baseDockerImage=vnmd/vesselboost_1.0.2
