c35547cbe887c90f181518282ed0013054084363
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-04-28 14:14:43 +1000

    use aws cli

--- .github/workflows/upload-zip.sh
+++ .github/workflows/upload-zip.sh
@@ -16,11 +16,13 @@ if [ -f "/var/lib/apt/lists/lock" ]; then
   sudo rm /var/lib/dpkg/lock*
 fi
 
-echo "[DEBUG] checking rclone config ..."
-cat ~/.config/rclone/rclone.conf
-
-echo "[DEBUG] Attempting upload to AWS Object Storage ..."
-rclone copy --progress $ZIPFILE aws-neurocontainers:/openrecon
+# check if aws cli is installed
+if ! command -v aws &>/dev/null; then
+  echo "[DEBUG] Installing AWS CLI"
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install && rm -rf aws awscliv2.zip
+fi
+time aws s3 cp $ZIPFILE s3://openrecon/${IMAGENAME}.zip
+echo "[DEBUG] Done with uploading to AWS Object Storage!"
 
 if curl --output /dev/null --silent --head --fail "https://openrecon.neurodesk.org/${IMAGENAME}.zip"; then
     echo "[DEBUG] ${IMAGENAME}.zip was freshly build and exists now :)"
