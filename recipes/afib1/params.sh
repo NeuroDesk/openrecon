e248754d3a5fb13d3a7292c8960c6a22cc35189f
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-05-23 08:40:33 +1000

    update to 1.6.0

--- recipes/afib1/params.sh
+++ recipes/afib1/params.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # specify the repostiory and name of the docker image
 export toolName=afib1
-export version=1.5.0
+export version=1.6.0
 export baseDockerImage=vnmd/${toolName}_${version}
